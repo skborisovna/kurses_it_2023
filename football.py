3b7a91d78a7c545d69851dcef5f228373e88b5a1
Svitlana Kushnarova <s.kushnareva@obdarovanist.kh.ua>
2023-03-23 17:23:34 +0200

    short message

---
+++ football.py
@@ -0,0 +1,9 @@
+ball = int(input('Vvedit vash ball'))
+if ball == 3:
+    print('Win!')
+elif ball == 0:
+    print("Loose!")
+elif ball == 1:
+    print('Draw!')
+else:
+    print('Vvedit pravylnuy ball')
